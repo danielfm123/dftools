aa1de89e399ccea7092d7e00b5e2d52e02b57839
Daniel Fischer <danielfm123@gmail.com>
2020-07-05 11:11:02 -0400

    fixed ROC

--- R/getROC.R
+++ R/getROC.R
@@ -15,21 +15,26 @@
 
 
 getROC = function(score, response, resolution = 1000){
-  dataset = data.frame(score, response)
-  dataset$slice = cut(dataset$score,
-                      quantile(dataset$score,seq(from = 0,to = 1,length.out = min(resolution,nrow(dataset) ) + 1 )),
-                      include.lowest = T)
-  dataset =  dataset %>% 
-    group_by(slice) %>% 
-    summarise(true_positive = sum(response),
-              false_positive = sum(!response))
-  dataset = dataset[order(dataset$slice,decreasing = T),]
-  
-  dataset = transform(
-    dataset,
-    true_positive = cumsum(true_positive) / sum(true_positive),
-    false_positive = cumsum(false_positive) / sum(false_positive),
-    identity = seq(from = 0, to = 1, length = nrow(dataset))
+  suppressWarnings({
+    dataset = data.frame(score, response)
+    dataset$slice = cut(dataset$score,
+                        unique(quantile(dataset$score,seq(from = 0,to = 1,length.out = min(resolution,nrow(dataset) ) + 1 ))),
+                        include.lowest = T)
+    dataset =  dataset %>% 
+      group_by(slice) %>% 
+      summarise(true_positive = sum(response),
+                false_positive = sum(!response))
+    dataset = dataset[order(dataset$slice,decreasing = T),]
+    
+    dataset = transform(
+      dataset,
+      true_positive = cumsum(true_positive) / sum(true_positive),
+      false_positive = cumsum(false_positive) / sum(false_positive)
+    )
+  })
+  dataset = bind_rows(
+    data.frame(slice = '0', true_positive = 0, false_positive = 0),
+    dataset %>% mutate(slice = as.character(slice))
   )
   return(dataset)
 }
