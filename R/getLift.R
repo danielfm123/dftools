ebe1f42927325d2eac29806a4c85f51d07d25869
Daniel Fischer <danielfm123@gmail.com>
2020-07-04 21:51:51 -0400

    suppress warnings

--- R/getLift.R
+++ R/getLift.R
@@ -14,29 +14,31 @@
 #' @author Daniel Fischer
 #' @export
 getLift = function(score, response, groups = 100){
-  
-  tasa_promedio = mean(response)
-  dataset = data.frame(score, response)
-  lift = dataset %>% 
-    arrange(-score) %>% 
-    mutate(percentil = cut(rank(score,ties.method = "first"),
-                       quantile(rank(score,ties.method = "first"),seq(from = 0, to = 1, length.out = groups + 1)),
-                       include.lowest = T,
-                       labels = seq(from = 100, to = 1, length.out = groups))) %>%
-    group_by(percentil) %>% 
-    summarise(min_score = min(score),
-              max_score = max(score),
-              mean_score = mean(score),
-              response_rate = mean(response),
-              detected = sum(response),
-              frequency = n()
-    ) %>% 
-    ungroup() %>% 
-    mutate(lift = response_rate/tasa_promedio,
-           percentil = as.numeric(as.character(percentil))) %>% 
-    arrange(percentil) %>% 
-    mutate(cum_response = cumsum(detected)/cumsum(frequency),
-           cum_lift = cum_response/tasa_promedio)
+  suppressWarnings({
+    tasa_promedio = mean(response)
+    dataset = data.frame(score, response)
+    lift = dataset %>% 
+      arrange(-score) %>% 
+      mutate(percentil = cut(rank(score,ties.method = "first"),
+                             quantile(rank(score,ties.method = "first"),seq(from = 0, to = 1, length.out = groups + 1)),
+                             include.lowest = T,
+                             labels = seq(from = 100, to = 1, length.out = groups))) %>%
+      group_by(percentil) %>% 
+      summarise(min_score = min(score),
+                max_score = max(score),
+                mean_score = mean(score),
+                response_rate = mean(response),
+                detected = sum(response),
+                frequency = n()
+      ) %>% 
+      ungroup() %>% 
+      mutate(lift = response_rate/tasa_promedio,
+             percentil = as.numeric(as.character(percentil))) %>% 
+      arrange(percentil) %>% 
+      mutate(cum_response = cumsum(detected)/cumsum(frequency),
+             cum_lift = cum_response/tasa_promedio)
+  })
+
   
   return(lift)
 }
