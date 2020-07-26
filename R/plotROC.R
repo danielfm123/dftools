e08841abd88da60c98f16a9cd72bf896ee01e95c
Daniel Fischer <danielfm123@gmail.com>
2020-07-26 11:16:52 -0400

    gridExtra

--- R/plotROC.R
+++ R/plotROC.R
@@ -41,6 +41,7 @@ plotROC = function(rocs = list(),...,show_auc = T){
     geom_abline(slope=1, intercept=0, size = 1.5)
   
   if(show_auc){
+    library(gridExtra)
     auc_data = getAUC(rocs)
     gr = gr + 
       annotation_custom(tableGrob(auc_data,rows=NULL), xmin=0.5, xmax=1, ymin=0, ymax=0.5)
