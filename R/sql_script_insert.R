f70c45619df74f8629fe14a00c3172622851d635
Ubuntu <alertador@vm-analytics.2pawshn5ofaetohdmvw3fqo32d.bx.internal.cloudapp.net>
2020-09-21 16:41:32 +0000

    fixed '

--- R/sql_script_insert.R
+++ R/sql_script_insert.R
@@ -4,7 +4,7 @@ sql_script_insert = function(datos,tabla){
   # print(datos)
   query = "INSERT INTO @tabla@ (@col@) VALUES (@val@)"
   query = sqlGsub(query,param=c(tabla = tabla, col = paste(colnames(datos),collapse=",") ))
-  values = datos %>% apply(1,function(x) paste(ifelse(is.na(x),"NULL",paste0("'",x,"'") ),collapse=","))
+  values = datos %>% apply(1,function(x) paste(ifelse(is.na(x),"NULL",paste0("'",str_replace_all(x,"\'","''''"),"'") ),collapse=","))
   
   values = sapply(values,function(x) sqlGsub(query,param = c(val = x) ))
   return(values)
